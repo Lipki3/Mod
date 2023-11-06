b447f580eba22f69dbbcf2b0138b9074742a4c03
Ryhor Lipski <73040453+Lipki3@users.noreply.github.com>
2023-11-06 15:27:06 +0300

    СМО

---
+++ lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(18, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Моделировать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0,5";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(137, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0,4";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(39, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(137, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "0,5";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ρ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "π1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "π2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(39, 102);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(137, 22);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "2";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "N";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(18, 152);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(158, 22);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "100000";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Кол-во моделирований";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(257, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Абсолютная пропускная способность";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(257, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(169, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Средняя длина очереди";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(318, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(318, 107);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(244, 166);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(197, 32);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "ВАРИАНТ 27";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(511, 245);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ lab3/lab3/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ro = 0;
+            if (textBox1.Text != "-")
+            {
+                ro = Convert.ToDouble(textBox1.Text); // Параметр геометрического распределения для входного потока
+            }
+            double pi1 = 0;
+            if (textBox2.Text != "-")
+            {
+                pi1 = Convert.ToDouble(textBox2.Text);  // Параметр геометрического распределения для времени обслуживания в π1
+            }
+            double pi2 = 0;
+            if (textBox3.Text != "-")
+            {
+                pi2 = Convert.ToDouble(textBox3.Text);  // Параметр геометрического распределения для времени обслуживания в π2
+            }
+            int N = 0;
+            if (textBox4.Text != "-")
+            {
+                N = Convert.ToInt32(textBox4.Text); // Число заявок в накопителе
+            }
+            int totalSimulations = 0;
+            if (textBox5.Text != "-")
+            {
+                totalSimulations = Convert.ToInt32(textBox5.Text); // Общее количество моделирований для сбора статистики
+            }
+
+          //  MessageBox.Show($"ro: {ro}, pi1: {pi1}, pi2: {pi2}, N: {N}, totalSimulations: {totalSimulations}");
+
+
+            int totalRequests = 0; // Общее число обработанных заявок
+            int queueLengthSum = 0; // Сумма длин очередей
+
+            Random random = new Random();
+
+            for (int i = 0; i < totalSimulations; i++)
+            {
+                int queueLength = 0;
+
+                // Моделирование прихода заявок и обслуживание
+                while (queueLength <= N)
+                {
+                    // Моделирование прихода заявки по геометрическому распределению
+                    if (random.NextDouble() > ro)
+                    {
+                        // Заявка пришла
+                        queueLength++;
+
+                        // Проверка, превышает ли очередь N, и отбрасывание заявок при необходимости
+                        if (queueLength > N)
+                        {
+                            queueLength--;
+                            break;
+                        }
+
+                        // Моделирование времени обслуживания в π1
+                        if (random.NextDouble() <= pi1)
+                        {
+                            queueLength--;
+
+                            // Проверка на отрицательное значение queueLength после обслуживания
+                            if (queueLength < 0)
+                            {
+                                queueLength = 0;
+                            }
+
+                            // Моделирование времени обслуживания в π2
+                            if (random.NextDouble() <= pi2)
+                            {
+                                queueLength--;
+
+                                // Проверка на отрицательное значение queueLength после обслуживания
+                                if (queueLength < 0)
+                                {
+                                    queueLength = 0;
+                                }
+
+                                // Обработанная заявка
+                                totalRequests++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Заявка не пришла
+                        break;
+                    }
+                }
+
+                queueLengthSum += queueLength;
+            }
+
+            double absoluteThroughput = (double)totalRequests / totalSimulations;
+            double averageQueueLength = (double)queueLengthSum / totalSimulations;
+
+            label8.Text = absoluteThroughput.ToString();
+            label9.Text = averageQueueLength.ToString();
+        }
+    }
+}
